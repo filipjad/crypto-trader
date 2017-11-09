0b68672c7c59fc7ccd914d22c5c42a031cd3442c
Adrian Bratteby <bratteby@kth.se>
2017-11-09 21:03:37 +0800

    fixed conflict3

--- tradebot.py
+++ tradebot.py
@@ -37,38 +37,6 @@ class Trader():
         os.system("say 'I am done Mother fucker let us trade some Ethereum'")
         print(self.past_prices)
 
-<<<<<<< HEAD
-    def fetch_data(self):
-        resp = requests.get("https://etherchain.org/api/statistics/price")
-        robj = json.loads(resp.text)
-
-        nrOfDays = 200
-        shortDays = 50
-        data = robj["data"][-nrOfDays*24:] #Get the last nrOfDays elements in list
-
-        nrOfElements = len(data)
-
-        longEma=[]  #Length of list 200 days*24  
-        shortEma=[] #Length of list 50 days*24
-
-        for i in range(nrOfElements):
-            element = data[i]
-            #print("Time:",element["time"],"Price:",element["usd"])
-            longEma.append(element["usd"])
-            if(i>=1200):
-                shortEma.append(element["usd"])
-
-        return shortEma, longEma
-
-
-    def calculate_emas(self):
-        window = len(self.past_prices)
-        self.weight = 2/(window+1)
-        self.past_prices.reverse()
-        prices = self.past_prices
-        for i in range(5):
-
-
     def fetch_data(self):
         resp = requests.get("https://etherchain.org/api/statistics/price")
         robj = json.loads(resp.text)
